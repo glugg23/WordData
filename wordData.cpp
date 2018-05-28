68288d2ef3a81522d6faa3dc7d107012982d6473
glugg23 <m.leonhardt424@gmail.com>
2018-05-28 12:44:38 +0100

    Using async now rather than thread

--- wordData.cpp
+++ wordData.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <thread>
 #include <future>
 
-//#include "word.h"
-
-void longestWordFunc(std::promise<std::string> &&longestWord, const std::map<std::string, int> &words) {
+std::string longestWordFunc(const std::map<std::string, int> &words) {
     std::string currentWord;
 
     for(auto &word : words) {
@@ -15,10 +12,10 @@ void longestWordFunc(std::promise<std::string> &&longestWord, const std::map<std
         }
     }
 
-    longestWord.set_value(currentWord);
+    return currentWord;
 }
 
-void shortestWordFunc(std::promise<std::string> &&shortestWord, const std::map<std::string, int> &words) {
+std::string shortestWordFunc(const std::map<std::string, int> &words) {
     //Large string to force a change when reading in the first word
     std::string currentWord = "1234567890123456789012345678901234567890123456789012345678901234567890";
 
@@ -28,10 +25,10 @@ void shortestWordFunc(std::promise<std::string> &&shortestWord, const std::map<s
         }
     }
 
-    shortestWord.set_value(currentWord);
+    return currentWord;
 }
 
-void getAverage(std::promise<double> &&average, const std::map<std::string, int> &words) {
+double getAverage(const std::map<std::string, int> &words) {
     int totalCharacters = 0;
     int totalWords = 0;
 
@@ -40,18 +37,18 @@ void getAverage(std::promise<double> &&average, const std::map<std::string, int>
         totalWords += word.second;
     }
 
-    average.set_value(static_cast<double>(totalCharacters) / totalWords);
+    return static_cast<double>(totalCharacters) / totalWords;
 }
 
-void mostCommonFunc(std::promise<std::tuple<std::string, int>> &&mostCommon, const std::map<std::string, int> &words) {
-    std::tuple<std::string, int> mostCommonTuple("", 0);
+std::tuple<std::string, int> mostCommonFunc(const std::map<std::string, int> &words) {
+    std::tuple<std::string, int> mostCommonTuple;
     for(auto &pair : words) {
         if(pair.second > std::get<int>(mostCommonTuple)) {
             mostCommonTuple = pair;
         }
     }
 
-    mostCommon.set_value(mostCommonTuple);
+    return mostCommonTuple;
 }
 
 int main() {
@@ -72,41 +69,21 @@ int main() {
 
     randomWords.close();
 
-    //Make a promise to return a string
-    std::promise<std::string> longestWordPromise;
-    //Set this value to go into here
-    auto longestWordFuture = longestWordPromise.get_future();
-    //Call the thread by moving the promise into it
-    std::thread longestWordThread(longestWordFunc, std::move(longestWordPromise), std::ref(wordMap));
-
-    std::promise<std::string> shortestWordPromise;
-    auto shortestWordFuture = shortestWordPromise.get_future();
-    std::thread shortestWordThread(shortestWordFunc, std::move(shortestWordPromise), std::ref(wordMap));
-
-    std::promise<double> averagePromise;
-    auto averageFuture = averagePromise.get_future();
-    std::thread averageThread(getAverage, std::move(averagePromise), std::ref(wordMap));
-
-    std::promise<std::tuple<std::string, int>> mostCommonPromise;
-    auto mostCommonFuture = mostCommonPromise.get_future();
-    std::thread mostCommonThread(mostCommonFunc, std::move(mostCommonPromise), std::ref(wordMap));
-
-    longestWordThread.join();
-    shortestWordThread.join();
-    averageThread.join();
-    mostCommonThread.join();
+    //Shows error "No matching function" but compiles and runs fine
+    auto longestWordThread = std::async(longestWordFunc, std::ref(wordMap));
+    auto shortestWordThread = std::async(shortestWordFunc, std::ref(wordMap));
+    auto averageThread = std::async(getAverage, std::ref(wordMap));
+    auto mostCommonThread = std::async(mostCommonFunc, std::ref(wordMap));
 
     //Get the value back from each future
-    std::string longestWord = longestWordFuture.get();
-    std::string shortestWord = shortestWordFuture.get();
-    double average = averageFuture.get();
-    std::tuple<std::string, int> mostCommon = mostCommonFuture.get();
+    std::string longestWord = longestWordThread.get();
+    std::string shortestWord = shortestWordThread.get();
+    double average = averageThread.get();
+    std::tuple<std::string, int> mostCommon = mostCommonThread.get();
 
     std::cout << "Longest word: " << longestWord << ", at " << longestWord.length() << " characters" << std::endl;
     std::cout << "Shortest word: " << shortestWord << ", at " << shortestWord.length() << " characters" << std::endl;
-
     std::cout << "Average length of words: " << average << std::endl;
-
     std::cout << "Most common word: " << std::get<std::string>(mostCommon) << ", at " << std::get<int>(mostCommon) << " occurrences" << std::endl;
 
     return 0;
